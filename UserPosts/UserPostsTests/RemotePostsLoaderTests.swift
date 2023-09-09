3c276ef625b73f001e1f2050acf04900c1c30d2d
Preetham <preethambaliga123@gmail.com>
2023-09-09 13:17:25 +0530

    updated tests file

--- UserPosts/UserPostsTests/RemotePostsLoaderTests.swift
+++ UserPosts/UserPostsTests/RemotePostsLoaderTests.swift
@@ -10,21 +10,4 @@ import UserPosts
 
 final class RemotePostsLoaderTests: XCTestCase {
 
-    func test_remotePostLoader_doesNotRequestDataFromURLUponCreation() {
-        let client = HTTPClientSpy()
-        let _ = RemotePostsLoader(client: client)
-
-        XCTAssertTrue(client.requestedURL.isEmpty)
-    }
-
-    // MARK: Helpers
-
-    class HTTPClientSpy: HTTPClient {
-
-        var requestedURL = [URL]()
-
-        func get(from url: URL) {
-            requestedURL.append(url)
-        }
-    }
 }
