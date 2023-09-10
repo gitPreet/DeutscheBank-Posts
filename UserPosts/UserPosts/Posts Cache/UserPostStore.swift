ce6bd2c5cf1f57d151ad6a4943c139760bd16df6
Preetham <preethambaliga123@gmail.com>
2023-09-10 14:28:38 +0530

    Added interface for UserPostStore

--- UserPosts/UserPosts/Posts Cache/UserPostStore.swift
+++ UserPosts/UserPosts/Posts Cache/UserPostStore.swift
@@ -7,4 +7,21 @@
 
 import Foundation
 
-protocol UserPostStore {}
+public enum RetrieveFavouritePostsResult {
+    case empty
+    case found(feed: [UserPost])
+    case failure(Error)
+}
+
+protocol UserPostStore {
+
+    typealias InsertionCompletion = (Error?) -> Void
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias RetrievalCompletion = (RetrieveFavouritePostsResult) -> Void
+
+    func insert(post: UserPost, completion: @escaping InsertionCompletion)
+
+    func delete(post: UserPost, completion: @escaping DeletionCompletion)
+
+    func retrieveFavouritePosts(completion: @escaping RetrievalCompletion)
+}
